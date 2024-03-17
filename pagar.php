3ee081b3fe5e86af364a9b88e09547d8220c59b7
Pablo Torrez <pablotorrez90@outlook.com>
2024-03-17 01:04:47 -0300

    diseño-de-pago

--- pagar.php
+++ pagar.php
@@ -7,13 +7,13 @@ include('templates/cabecera.php');
 
 <?php
 
-if($_POST) {
+if ($_POST) {
     $total = 0;
     $SID = session_id(); // recupero la session del id, esta funcion devuelve la clave de la sesión. (evitar confusion con otro pedido)
     $Correo = $_POST['email'];
 
-    foreach($_SESSION['CARRITO'] as $indice=>$producto){ 
-        $total = $total + ($producto['PRECIO']*$producto['CANTIDAD']);
+    foreach ($_SESSION['CARRITO'] as $indice => $producto) {
+        $total = $total + ($producto['PRECIO'] * $producto['CANTIDAD']);
     }
 
     $sentenciaSQL = $pdo->prepare(
@@ -27,7 +27,7 @@ if($_POST) {
     $sentenciaSQL->execute();
     $idVenta = $pdo->lastInsertId();
 
-    foreach($_SESSION['CARRITO'] as $indice=>$producto){
+    foreach ($_SESSION['CARRITO'] as $indice => $producto) {
         $sentenciaSQL = $pdo->prepare(
             "INSERT INTO `tbldetalleventa` (`ID`, `idVenta`, `idProducto`, `preciounitario`, `cantidad`, `descargado`)
             VALUES(NULL, :IDVENTA, :IDPRODUCTO, :PRECIOUNITARIO, :CANTIDAD, '0');"
@@ -40,9 +40,20 @@ if($_POST) {
         $sentenciaSQL->execute();
     }
 
-    echo "<h3>".$total."</h3>";
+    // echo "<h3>".$total."</h3>";
 }
 
 ?>
+
+<div class="jumbotron text-center mt-5 bg-success-subtle p-5">
+    <h1 class="display-4">¡Paso final!</h1>
+    <hr class="my-4">
+    <p class="lead">Estás a punto de pagar con PayPal la cantidad de:
+    <h4><?php echo number_format($total, 2); ?></h4>
+    </p>
+    <p>Los productos podrán ser descargados una vez se procese el pago
+        <strong>(Para aclaraciones: webdesign@gmail.com)</strong>
+    </p>
+</div>
 
 <?php include('templates/pie.php'); ?>
